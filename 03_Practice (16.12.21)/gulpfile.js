64e962317195c95f5913d6d87e6b44a5d07270af
Nataliia Sychevska <petriwna43@gmail.com>
2021-12-17 01:59:31 +0200

    create calendar

---
+++ 03_Practice (16.12.21)/gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+
+gulp.task('clean', function(cb) {
+    del(['dist/*']);
+    cb();
+})
+
+gulp.task('sass', function(cb) {
+    gulp.src('./src/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist'));
+    cb();
+})
+
+gulp.task('copy:html', function (cb){
+    gulp.src('./src/index.html')
+        .pipe(gulp.dest('./dist'))
+    cb();
+})
+
+gulp.task('watch', function () {
+    gulp.watch(['./src/**/*.scss', './src/**/*.html'], gulp.series(['clean', 'sass', 'copy:html']));
+})
+
+gulp.task('default', gulp.series(['clean', 'sass', 'copy:html']))
